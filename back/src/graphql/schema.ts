082bb4a97cb3895d871d85cba43c8d59519a482d
Mariano Wadel <mwadel@addi.com>
2023-09-28 17:28:46 -0300

    Added graphql schemas and resolvers

--- back/src/graphql/schema.ts
+++ back/src/graphql/schema.ts
@@ -1,3 +1,25 @@
 import { buildSchema } from 'graphql';
 
-export const schema = buildSchema(``);
+export const schema = buildSchema(`
+
+  type Product {
+    id: ID!
+    name: String!
+    picture: String!
+    description: String!
+    price: Float!
+  }
+  
+  type Delivery {
+    id: ID!
+    date: String!
+    products: [Product!]! # Add a products field to Delivery
+  }
+  
+  type Query {
+    deliveries: [Delivery!]!
+    products: [Product!]!
+    delivery(id: ID!): Delivery 
+    product(id: ID!): Product
+  }
+`);
--- back/src/server.ts
+++ back/src/server.ts
@@ -1,17 +1,22 @@
 import express from 'express';
 import { graphqlHTTP } from 'express-graphql';
 import { schema } from './graphql/schema';
-import { fetchDeliveries } from './graphql/resolver';
+import { fetchDeliveries, fetchDeliveryWithProducts } from './graphql/resolver';
+import cors from 'cors';
 
 function createServer() {
   const server = express();
+
+  server.use('/graphql', cors());
 
   server.use(
     '/graphql',
     graphqlHTTP({
       schema: schema,
+      context: {},
       rootValue: {
-        fetchDeliveries,
+        deliveries: fetchDeliveries,
+        delivery: fetchDeliveryWithProducts,
       },
       graphiql: true,
     })
