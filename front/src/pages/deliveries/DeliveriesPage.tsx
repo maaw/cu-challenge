2960ea33f546c218f5735bfe59b11cf393968744
Mariano Wadel <mwadel@addi.com>
2023-09-28 17:30:32 -0300

    Updated components

--- front/src/pages/deliveries/DeliveriesPage.tsx
+++ front/src/pages/deliveries/DeliveriesPage.tsx
@@ -1,12 +1,35 @@
-import { FC } from 'react';
+import { FC, useState } from 'react';
 import { Header } from './Header/Header';
 import { Main } from './Main/Main';
+import Delivery from '../../models/Delivery';
+import GET_DELIVERIES from '../../graphql/queries';
+import { useQuery } from '@apollo/client';
 
 export const DeliveriesPage: FC = () => {
+  const [selectedDelivery, setSelectedDelivery] = useState<Delivery | any>();
+
+  //Get deliveries from graphql API
+  const { loading, error, data } = useQuery(GET_DELIVERIES);
+
+  if (loading) return <p>Loading...</p>;
+  if (error) return <p>Error: {error.message}</p>;
+
+  const deliveries = data.deliveries;
+
   return (
     <>
-      <Header />
-      <Main />
+      <Header
+        deliveries={deliveries}
+        selectedDelivery={selectedDelivery}
+        setSelectedDelivery={setSelectedDelivery}
+      />
+      <Main
+        deliveryId={
+          selectedDelivery === undefined
+            ? deliveries[0].id
+            : selectedDelivery.id
+        }
+      />
     </>
   );
 };
--- front/src/pages/deliveries/Header/Header.tsx
+++ front/src/pages/deliveries/Header/Header.tsx
@@ -1,28 +1,62 @@
-import { FC } from 'react';
 import './Header.scss';
+import Delivery from '../../../models/Delivery';
+import { isHoliday } from 'date-fns-holiday-us';
 
-export const Header: FC = () => {
+interface HeaderProps {
+  deliveries: Delivery[];
+  selectedDelivery: Delivery | any;
+  setSelectedDelivery: (delivery: Delivery | any) => void;
+}
+
+export const Header: React.FC<HeaderProps> = ({
+  deliveries,
+  selectedDelivery,
+  setSelectedDelivery,
+}) => {
+  const handleTabClick = (delivery: Delivery) => {
+    setSelectedDelivery(delivery);
+    // Scroll to the top of the page
+    window.scrollTo(0, 0);
+  };
+
+  const formatDate = (dateString: string) => {
+    const options = {
+      weekday: 'long',
+      month: 'long',
+      day: 'numeric',
+    };
+    const date = new Date(dateString);
+    return date.toLocaleDateString(undefined, options);
+  };
+
+  const shouldReschedule = isHoliday(
+    new Date(selectedDelivery?.date || deliveries[0].date)
+  );
+
   return (
     <header className="header-container">
       <div className="page__horizontal-space header">
         <div className="tabs-container">
           <div className="tabs">
-            <button className="tab active">
-              <span className="date">Monday, August 23</span>
-            </button>
-            <button className="tab active">
-              <span className="date">Monday, August 23</span>
-            </button>
-            <button className="tab active">
-              <span className="date">Monday, August 23</span>
-            </button>
+            {deliveries.map((delivery) => (
+              <button
+                className={`tab ${
+                  delivery.id === (selectedDelivery?.id || deliveries[0]?.id)
+                    ? 'active'
+                    : ''
+                }`}
+                key={delivery.id}
+                onClick={() => handleTabClick(delivery)}>
+                <span className="date">{formatDate(delivery.date)}</span>
+              </button>
+            ))}
           </div>
         </div>
         <button
           className="btn-primary btn-buy"
           data-test-id="header-cta"
-          onClick={() => alert('Confirmed')}>
-          Confirm
+          onClick={() => alert(shouldReschedule ? 'Rescheduled' : 'Confirmed')}>
+          {shouldReschedule ? 'Reschedule' : 'Confirm'}
         </button>
       </div>
     </header>
